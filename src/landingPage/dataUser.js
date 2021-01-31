541125d7e23b085e9273da4976f56ff01c8aa027
Angga Afrianto <anggaaurelius@gmail.com>
2021-01-31 19:07:19 +0700

    fungsi login dan register

--- src/landingPage/dataUser.js
+++ src/landingPage/dataUser.js
@@ -1,11 +1,9 @@
 export const Users = [
         {
             "id" : 1,
-            "fullname" : "Deddy Rahmat",
-            "email" : "deddy@gmail.com",
-            "password" : "12345678",
-            "avatar" : "https://images.unsplash.com/photo-1536111873776-77ca66928e33?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60",
-            "role" : "admin"
+            "fullname" : "Angga Afrianto",
+            "email" : "anggaaurelius@gmail.com",
+            "password" : "123",
         },
         {
             
@@ -13,8 +11,6 @@ export const Users = [
             "fullname" : "Dinda Ramadhani",
             "email" : "dinda@gmail.com",
             "password" : "12345678",
-            "avatar" : "https://images.unsplash.com/photo-1557173137-35edbf78d46c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60",
-            "role" : "user"
         },
         {
             
@@ -22,8 +18,6 @@ export const Users = [
             "fullname" : "Budi Sudarso",
             "email" : "budi@gmail.com",
             "password" : "12345678",
-            "avatar" : "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60",
-            "role" : "user"
         },
         {
             
@@ -31,8 +25,6 @@ export const Users = [
             "fullname" : "Fatimah Zahra",
             "email" : "fatimah@gmail.com",
             "password" : "12345678",
-            "avatar" : "https://images.unsplash.com/photo-1550480082-dd9c35032f45?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-            "role" : "user"
         }
 
     ]
