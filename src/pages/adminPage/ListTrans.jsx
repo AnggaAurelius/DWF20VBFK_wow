f4ed3f026cc32ac5e2e7c4a8544d5c2fcdb96319
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-14 10:58:11 +0700

    approve/ cancel

--- src/pages/adminPage/ListTrans.jsx
+++ src/pages/adminPage/ListTrans.jsx
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './table.css';
 import aksi from './image/aksi.png';
-import {Transaction} from '../adminPage/dataTransaction';
-import {Dropdown} from 'react-bootstrap';
+import { API } from "../../config/api";
 import AdminNavbar  from './Navbar';
+import { Button, Modal } from 'react-bootstrap';
 
 
 export const LisTrans = () => {
-    return ( 
-        <div className="bgt full" >
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const [image, setImg] = useState("");
+    const handleShow = (img) => {
+        setImg(img);
+        setShow(true);
+    }
+    let no = 1;
+    const [loading, setLoading] = useState(true);
+    const [transactions, serTransactions] = useState([]);
+    const getTransaction = async () => {
+        try {
+            setLoading(true);
+            const transactions = await API.get("/transactions");
+            setLoading(false);
+            serTransactions(transactions.data.data.transactions);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getTransaction();
+    }, [])
+
+    const payment = async (id,action) => {
+
+        try {
+        const body = JSON.stringify({
+            paymentStatus: action,
+        });
+
+        const config = {
+            headers: {
+            "Content-Type": "application/json",
+            },
+        };
+
+        setLoading(true);
+
+        await API.patch(`/transaction/${id}`, body, config);
+
+        setLoading(false);
+
+        getTransaction();
+
+        } catch (error) {
+        console.log(error);
+        }
+    };
+    return loading ? (
+        <h1>loading</h1>
+        ) :  ( 
+        <div className="bgt " >
             <AdminNavbar />
                 <div className="mlr " >
                     <h1 className="mbot timesNew">Incoming Transaction</h1>
@@ -16,8 +67,8 @@ export const LisTrans = () => {
                         <thead>
                             <tr className="red">
                             <th scope="co">No</th>
-                            <th scope="col">Users</th>
-                            <th scope="col">Bukti Transfer</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Transfer Proof</th>
                             <th scope="col">Remaining Active</th>
                             <th scope="col">Status User</th>
                             <th scope="col">Status Payment</th>
@@ -25,33 +76,37 @@ export const LisTrans = () => {
                             </tr>
                         </thead>
                          <tbody >
-                        {Transaction.map((Transaction) => (
-                         
+                        {transactions.map((Transaction) => (
                             <tr key={Transaction.id}>
-                            <th scope="row">{Transaction.id}</th>
-                            <td>{Transaction.name}</td>
-                            <td>{Transaction.bukti}</td>
-                            <td>{Transaction.aktif}</td>
-                            <td className={`${Transaction.status == "Active" ? "green" : "red" }`}>{Transaction.status}</td>
-                            <td className={`${Transaction.pay == "Approve" ? "text-info" : Transaction.pay == "Cancel" ? "red" : "text-warning" }`}>{Transaction.pay}</td>
+                            <th scope="row">{no++}</th>
+                            <td>{Transaction.users.fullName}</td>
+                            <td><Button className="ml-3" variant="success" onClick={() => handleShow(Transaction.transferProof) }>Image</Button>
+                            </td>
+                            <td>{Transaction.remainingActive}</td>
+                            <td className={`${Transaction.userStatus == "active" ? "approve" : "red" }`}>{Transaction.userStatus}</td>
+                            <td className={`${Transaction.paymentStatus == "Approve" ? "approve" : Transaction.paymentStatus == "Cancel" ? "red" : "text-warning" }`}>{Transaction.paymentStatus}</td>
                             <td>
-                                <div className="dropdown">  
-                                <button className="btn">  <img className="" src={aksi} alt="" /></button>  
-                                <div className="dropdown-content">  
-                                <a href=""><p className="green bold">Approval</p></a>
-                                <a href=""><p className="red bold">Cancel</p></a>
+                                <div className="dropdown aksi">  
+                                    <img className="" src={aksi} alt="" /> 
+                                <div className="dropdown-content"> 
+                                    <div className="">
+                                        <p className="approve bold center " onClick={() => payment(Transaction.id,"Approve")}>Approved</p>
+                                    </div> 
+                                    <div className="">
+                                        <p className="red bold center" onClick={() => payment(Transaction.id,"Cancel")}>Cancel</p>
+                                    </div>
                                 </div>  
                                 </div>  
                                 </td>
                             </tr>
-                           
+                            
                         ))}
                         </tbody>
-                        
-                        </table>
+                    </table>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Body className="center"><img src={`http://localhost:5000/uploads/${image}`} /></Modal.Body>
+                    </Modal>
                 </div>
-                
-            
         </div>
     )
 }
